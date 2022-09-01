93cc9399bf472401eb8d5c43aae5c39a71a47e55
Vikrant Kataria <vik7am@gmail.com>
2022-09-01 13:54:47 +0530

    Added CCTV Script

---
+++ Assets/Scripts/CCTV.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CCTV : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        PlayerMovement player = other.GetComponent<PlayerMovement>();
+        if(player != null){
+            player.SetStopMovement(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        PlayerMovement player = other.GetComponent<PlayerMovement>();
+        if(player != null){
+            player.SetStopMovement(false);
+        }
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,16 +5,22 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     Vector2 velocity;
+    Vector2 oldVelocity;
     Rigidbody2D body;
     [SerializeField] float speed;
     float x, y;
+    Camera cam;
+    bool stopMovement;
 
     void Awake(){
         body = GetComponent<Rigidbody2D>();
+        cam = Camera.main;
     }
 
     void Update()
     {
+        if(stopMovement)
+            return;
         GetPlayerInput();
         UpdatePlayerRotation();
     }
@@ -23,12 +29,19 @@ public class PlayerMovement : MonoBehaviour
         body.velocity = velocity * speed;
     }
 
+    void LateUpdate() {
+        cam.transform.position = new Vector3(transform.position.x, transform.position.y , -10);
+    }
+
     void GetPlayerInput(){
         velocity.x = Input.GetAxisRaw("Horizontal");
         velocity.y = Input.GetAxisRaw("Vertical");
     }
 
     void UpdatePlayerRotation(){
+        if(oldVelocity == velocity)
+            return;
+        oldVelocity = velocity;
         if(velocity.x == 1)
             transform.localRotation = Quaternion.Euler(0, 0, 0);
         else if(velocity.x == -1)
@@ -37,5 +50,10 @@ public class PlayerMovement : MonoBehaviour
             transform.localRotation = Quaternion.Euler(0, 0, 90);
         else if(velocity.y == -1)
             transform.localRotation = Quaternion.Euler(0, 0, -90);
+    }
+
+    public void SetStopMovement(bool status){
+        velocity = Vector2.zero;
+        stopMovement = status;
     }
 }
